32da665e5c6c904e859fb572037885382abeef80
Lord-Haji <lordhajisinbox@gmail.com>
2023-12-29 11:35:51 +0530

    Bleeding Edge Installer
    Add descriptive print and fix typo
    
    Change download branch from test to main

--- simple_ffmpeg.sh
+++ simple_ffmpeg.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 
-BASE_URL="https://johnvansickle.com/ffmpeg/releases"
+RELEASE_URL="https://johnvansickle.com/ffmpeg/releases"
+GIT_URL="https://johnvansickle.com/ffmpeg/builds"
 
 install_ffmpeg() {
-    local arch=$1
-    local tarball="ffmpeg-release-${arch}-static.tar.xz"
-    local download_url="${BASE_URL}/${tarball}"
+    local url=$1
+    local arch=$2
+    local build_type=$3
+    local tarball="ffmpeg-${build_type}-${arch}-static.tar.xz"
+    local download_url="${url}/${tarball}"
     local temp_dir=$(mktemp -d) || { echo "Failed to create temp directory"; exit 1; }
 
-    echo "Downloading ffmpeg for ${arch}..."
+    # Improved print statement
+    local build_desc="release"
+    if [ "$build_type" == "git" ]; then
+        build_desc="latest Git"
+    fi
+    echo "Downloading the $build_desc build of ffmpeg for ${arch}..."
+
     wget $download_url -P "$temp_dir" || { echo "Download failed"; exit 1; }
 
     tar -xf "$temp_dir/$tarball" -C "$temp_dir" || { echo "Extraction failed"; exit 1; }
@@ -29,10 +38,19 @@ install_ffmpeg() {
 
 arch=$(uname -m)
 
+# Determine the URL and build type based on the flag
+url=$RELEASE_URL
+build_type="release"
+if [[ "$1" == "-g" ]] || [[ "$1" == "--git" ]]; then
+    url=$GIT_URL
+    build_type="git"
+fi
+
+# Install ffmpeg based on the architecture and build type
 case $arch in
-    x86_64|amd64) install_ffmpeg "amd64" ;;
-    i686) install_ffmpeg "i686" ;;
-    armv7l) install_ffmpeg "armhf" ;;
-    aarch64) install_ffmpeg "arm64" ;;
+    x86_64|amd64) install_ffmpeg $url "amd64" $build_type ;;
+    i686) install_ffmpeg $url "i686" $build_type ;;
+    armv7l) install_ffmpeg $url "armhf" $build_type ;;
+    aarch64) install_ffmpeg $url "arm64" $build_type ;;
     *) echo "Unsupported architecture: $arch"; exit 1 ;;
 esac
